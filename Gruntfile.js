b95f55c6dd95603b451f30f5ea9b4ff852351cd0
Alex Monk <krenair@gmail.com>
2019-08-25 02:48:47 +0100

    Split apihelp messages to a separate file
    Bug: T189982
    Change-Id: I207ed1012b22372379ae7f3255669d52f151847f

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,10 @@
 module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
-		banana: {
-			all: [
-				'i18n/',
-			]
-		},
+		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
 				'**/*.json',
